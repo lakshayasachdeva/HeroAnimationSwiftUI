f005bf11ebeb3f210edc23b0b04ebbb6a9695746
Lakshaya Sachdeva <lakshaya.sachdeva@colearn.id>
2023-10-17 16:36:36 +0530

    Initial Commit

---
+++ HeroAnimationSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HeroAnimationSwiftUI
+//
+//  Created by Lakshaya Sachdeva on 17/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HeroAnimationSwiftUI/HeroAnimationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroAnimationSwiftUIApp.swift
+//  HeroAnimationSwiftUI
+//
+//  Created by Lakshaya Sachdeva on 17/10/23.
+//
+
+import SwiftUI
+
+@main
+struct HeroAnimationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
